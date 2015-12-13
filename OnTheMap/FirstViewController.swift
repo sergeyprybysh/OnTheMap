bff4fa845087a492063029f634779efdf9047216
alekspribysh <sergeyprybysh@gmail.com>
2015-12-12 23:42:39 -0800

    Initial Commit

---
+++ OnTheMap/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OnTheMap
+//
+//  Created by Sergey Prybysh on 12/12/15.
+//  Copyright © 2015 Sergey Prybysh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OnTheMap/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OnTheMap
+//
+//  Created by Sergey Prybysh on 12/12/15.
+//  Copyright © 2015 Sergey Prybysh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
